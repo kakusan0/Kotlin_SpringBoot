f64f7f97f2e027614e64b1a6aaae430633eecba4
akihiro <gmakihiro@gmail.com>
2025-09-29 03:54:19 +0900

    Delete target directory

--- target/classes/db/migration/V6__add_path_name.sql
+++
@@ -1,4 +0,0 @@
--- Add path_name column to content_items
-ALTER TABLE content_items ADD COLUMN IF NOT EXISTS path_name VARCHAR(255) DEFAULT '';
-CREATE INDEX IF NOT EXISTS idx_path_name ON content_items(path_name);
-
