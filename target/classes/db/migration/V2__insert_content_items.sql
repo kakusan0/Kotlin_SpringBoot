86a745e127bac7fb2ef1f81bd4114e3b72d172d7
akihiro <gmakihiro@gmail.com>
2025-10-05 10:22:34 +0900

    a

--- target/classes/db/migration/V2__insert_content_items.sql
+++ target/classes/db/migration/V2__insert_content_items.sql
@@ -1,7 +1,7 @@
 -- V2__insert_content_items.sql
 -- Insert initial content items
-INSERT INTO content_items (item_name)
+INSERT INTO content_items (item_name, menu_name, path_name, enabled)
 VALUES
-  ('ホーム'),
-  ('パスワード生成'),
-  ('設定');
+  ('ホーム', 'ホーム', 'home', TRUE),
+  ('パスワード生成', 'ツール', 'pwgen', TRUE),
+  ('設定', '設定', 'settings', TRUE);
