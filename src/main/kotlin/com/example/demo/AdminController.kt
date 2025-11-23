5dd300eff92bc1d2ac69edc7757255976ca5b9e0
akihiro <gmakihiro@gmail.com>
2025-11-23 12:01:22 +0900

    あ

--- src/main/kotlin/com/example/demo/AdminController.kt
+++ src/main/kotlin/com/example/demo/AdminController.kt
@@ -4,29 +4,19 @@ import com.example.demo.mapper.AccessLogMapper
 import com.example.demo.mapper.BlacklistIpMapper
 import com.example.demo.mapper.UaBlacklistRuleMapper
 import com.example.demo.mapper.WhitelistIpMapper
-import com.example.demo.service.ContentItemService
-import com.example.demo.service.MenuService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class AdminController(
-    private val contentItemService: ContentItemService,
-    private val menuService: MenuService,
     private val whitelistIpMapper: WhitelistIpMapper,
     private val blacklistIpMapper: BlacklistIpMapper,
     private val uaBlacklistRuleMapper: UaBlacklistRuleMapper,
     private val accessLogMapper: AccessLogMapper,
 ) {
     @GetMapping("/manage")
-    fun manage(model: Model): String {
-        model.apply {
-            addAttribute("screens", contentItemService.getAll())
-            addAttribute("menus", menuService.getAll())
-            addAttribute("whitelist", whitelistIpMapper.getActive())
-            addAttribute("blacklist", blacklistIpMapper.getAll())
-        }
+    fun manage(): String {
         return "manage"
     }
 
